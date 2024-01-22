cbb1b7fa819294d517a18af495e217a192943dca
chzky <hylk453030@gmail.com>
2024-01-22 11:05:45 +0800

    feat(Gym): add type

--- lib/Gymnastics/mod.ts
+++ lib/Gymnastics/mod.ts
@@ -1,3 +1,5 @@
 export type ValueOrFunc<T> = T | (() => T)
 export type JudeCondition<T> = (val: T) => boolean
 export type Condition<T> = T | JudeCondition<T>
+
+export type Func<T, R> = (val: T) => R
