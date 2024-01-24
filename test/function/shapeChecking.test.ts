0d9dc0096bd4523899c0a73c4d8158a334a2cacf
chzky <hylk453030@gmail.com>
2024-01-24 11:04:53 +0800

    feat(function): add shape_checking

---
+++ test/function/shapeChecking.test.ts
@@ -0,0 +1,20 @@
+import {shape_checking, zod} from 'lib'
+import {assert, assertEquals} from '../mod.ts'
+
+Deno.test('shape-checking', () => {
+  const res = shape_checking({a: 1}, zod.object({a: zod.number()}))
+  const res1 = shape_checking({a: 1}, zod.object({a: zod.string()}))
+
+  const res2 = shape_checking({a: 1}, e => {
+    if (e.a === 1) throw 'Error' // 任何 throw 都被作为错误错误信息返回
+    return true // 只要返回true就是通过,返回false就是不通过,错误信息为 'shape_checking failed'
+  })
+  const res3 = shape_checking({a: 1}, e => e.a === 1)
+  assertEquals({a: 1}, res.unwarp())
+  assert(res1.is_err)
+
+  assertEquals({a: 1}, res3.unwarp())
+  res2.match_err(e => {
+    assertEquals('Error', e)
+  })
+})
