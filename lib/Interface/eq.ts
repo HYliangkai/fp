9d312fa12f377d54dae1c028610800cadf47762e
chzky <hylk453030@gmail.com>
2024-05-13 10:45:18 +0800

    ✨ feat(0.6.6): update
    + add interface doc

--- lib/Interface/eq.ts
+++ lib/Interface/eq.ts
@@ -1,4 +1,18 @@
 /** ## Eq : 提供一个不同类型数据相等性判断的功能
+@example
+  ```ts
+  class A implements Eq<number> {
+    constructor(public a?: number) {}
+
+    equals(val: number): boolean {
+      return this.a === val
+    }
+  }
+  const Na = new A(1)
+  assert(Na.equals(1) === true)
+  assert(Na.equals(2) === false)
+  ```
+@category Interface
  */
 
 export interface Equal<A> {
--- lib/Interface/ord.ts
+++ lib/Interface/ord.ts
@@ -1,5 +1,36 @@
 import {Equal} from './mod.ts'
 
-/** ## Ord : 提供排序比较的功能
+/** ## OrdResult : 比较结果
+@case -1 小于
+@case 0 等于
+@case 1 大于
+@category Interface
  */
-export interface Ord<T> extends Equal<T> {}
+type OrdResult = -1 | 0 | 1
+
+/** ## Ord : 提供排序比较的功能
+@example
+```ts
+class A implements Ord<number> {
+  constructor(public a?: number) {}
+
+  equals(val: number): boolean {
+    return this.a === val
+  }
+  compare(val: number): number {
+    return this.a - val
+  }
+}
+const Na = new A(1)
+assert(Na.equals(1) === true)
+assert(Na.equals(2) === false)
+assert(Na.compare(2) === -1)
+assert(Na.compare(1) === 0)
+assert(Na.compare(0) === 1)
+```
+@category Interface
+
+ */
+export interface Ord<T> extends Equal<T> {
+  compare(val: T): OrdResult
+}
