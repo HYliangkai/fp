dc5209326d5bbf0a665d008cd0518c9570fc78d0
陈泽楷 <chenzekai@njxinpu.com.cn>
2024-07-04 22:04:12 +0800

    ✨ feat(ext): add a-calc

---
+++ lib/Ext/aCalc.ts
@@ -0,0 +1,12 @@
+/** 
+[a-calc文档](https://github.com/Autumn-one/a-calc-old/blob/main/README_ZH.md)
+a-calc是一个js计算库,解决js计算精度问题,然后还附带了一些常用的计算方法
+  
+  使用场景:
+  1. 处理js计算精度问题
+  2. 提供常用的计算方法
+@category ext
+*/
+import * as a_calc from 'npm:a-calc'
+
+export { a_calc }
